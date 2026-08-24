b15513e9c87c420f4d80a0c461fa297814f00546
ILoveOpenSourceApplications <117499019+ILoveOpenSourceApplications@users.noreply.github.com>
2026-08-24 13:34:43 +0530

    chore(Theme): Fix typo

--- patches/src/main/kotlin/app/morphe/patches/youtube/layout/theme/ThemePatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/layout/theme/ThemePatch.kt
@@ -242,7 +242,7 @@ val themePatch = baseThemePatch(
                         tag = "app.morphe.extension.shared.theme.ThemeColorListPreference"
                     ),
                     TextPreference(
-                        "morphe_theme_color_custom_dark",
+                        "morphe_theme_color_dark_custom",
                         tag = "app.morphe.extension.shared.settings.preference.ColorPickerPreference",
                         inputType = InputType.TEXT_CAP_CHARACTERS
                     ),
@@ -251,7 +251,7 @@ val themePatch = baseThemePatch(
                         tag = "app.morphe.extension.shared.theme.ThemeColorListPreference"
                     ),
                     TextPreference(
-                        "morphe_theme_color_custom_light",
+                        "morphe_theme_color_light_custom",
                         tag = "app.morphe.extension.shared.settings.preference.ColorPickerPreference",
                         inputType = InputType.TEXT_CAP_CHARACTERS
                     ),
